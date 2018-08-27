57501267f982c1de495ae1d4feb147df6a578534
Stephen Karger <stephen.karger@gmail.com>
2018-08-27 18:45:46 -0400

    Rubocop

--- spec/rubocop/cop/salsify/style_dig_spec.rb
+++ spec/rubocop/cop/salsify/style_dig_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 describe RuboCop::Cop::Salsify::StyleDig, :config do
+  let(:ruby_version) { 2.3 }
   let(:msgs) { [described_class::MSG] }
 
   subject(:cop) { described_class.new(config) }
-  let(:ruby_version) { 2.3 }
 
   it "accepts non-nested access" do
     source = 'ary[0]'
