f247991e42b7108267c1ee034b5ed8e7e579e943
Keith Barrette <896780+kbarrette@users.noreply.github.com>
2022-09-21 07:33:03 -0400

    Clean up patch

--- lib/rubocop/cop/salsify/i18n.rb
+++ lib/rubocop/cop/salsify/i18n.rb
@@ -4,27 +4,22 @@ require 'rubocop-i18n'
 
 module RuboCop
   module Cop
-    module I18n
-      module RailsI18n
-        class DecorateString < Cop
-          # Original: https://github.com/puppetlabs/rubocop-i18n/blob/v3.0.0/lib/rubocop/cop/i18n/rails_i18n/decorate_string.rb#L138
-          # This patch adds the parent_is_logger? check
-          def check_for_parent_decorator(node)
-            return if parent_is_translator?(node.parent)
-            return if parent_is_indexer?(node.parent)
-            return if parent_is_logger?(node.parent)
-            return if ignoring_raised_parent?(node.parent)
+    module Salsify
+      module RailsI18nPatch
+        def check_for_parent_decorator(node)
+          return if parent_is_logger?(node.parent)
 
-            add_offense(node, message: 'decorator is missing around sentence')
-          end
+          super(node)
+        end
 
-          private
+        private
 
-          def parent_is_logger?(parent)
-            parent.respond_to?(:method_name) && [:debug, :info, :warn, :error].include?(parent.method_name)
-          end
+        def parent_is_logger?(parent)
+          parent.respond_to?(:method_name) && [:debug, :info, :warn, :error].include?(parent.method_name)
         end
       end
     end
   end
 end
+
+RuboCop::Cop::I18n::RailsI18n::DecorateString.prepend(RuboCop::Cop::Salsify::RailsI18nPatch)
--- spec/rubocop/cop/salsify/i18n_spec.rb
+++ spec/rubocop/cop/salsify/i18n_spec.rb
@@ -3,17 +3,24 @@
 require 'rubocop/cop/salsify/i18n'
 
 describe RuboCop::Cop::I18n::RailsI18n::DecorateString, :config do
+  let(:cop_config) { { 'EnforcedSentenceType' => 'fragment' } }
+
   subject(:cop) { described_class.new(config) }
 
   it "ignores loggers" do
     source = 'Rails.logger.info("some string")'
+
     inspect_source(source)
+
     expect(cop.offenses).to be_empty
   end
 
   it "does not ignore other methods" do
     source = 'puts("some string")'
+
     inspect_source(source)
-    expect(cop.offenses).to be_empty
+
+    expect(cop.offenses).not_to be_empty
+    expect(cop.messages.first).to include('decorator is missing')
   end
 end
