dd9fad01dfa73666c240dfa081abe4cef0a2bbd8
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-26 01:49:31 +0000

    Update tests

--- spec/rubocop/cop/salsify/rails_application_mailer_spec.rb
+++ spec/rubocop/cop/salsify/rails_application_mailer_spec.rb
@@ -3,6 +3,8 @@
 describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   let(:highlights) { 'ActionMailer::Base' }
 
+  let(:message) { "#{cop.cop_name}: #{described_class::MSG}" }
+
   subject(:cop) { described_class.new }
 
   it "allows ApplicationMailer to be defined" do
@@ -14,7 +16,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects mailers that subclass ActionMailer::Base" do
     source = "class MyMailer < ActionMailer::Base\nend"
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq("class MyMailer < ApplicationMailer\nend")
   end
@@ -22,7 +24,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects single-line class definitions" do
     source = 'class MyMailer < ActionMailer::Base; end'
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq('class MyMailer < ApplicationMailer; end')
   end
@@ -30,7 +32,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects namespaced mailers that subclass ActionMailer::Base" do
     source = "module Nested\n  class MyMailer < ActionMailer::Base\n  end\nend"
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq("module Nested\n  class MyMailer < ApplicationMailer\n  end\nend")
   end
@@ -38,7 +40,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects mailers defined using nested constants" do
     source = "class Nested::MyMailer < ActionMailer::Base\nend"
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq("class Nested::MyMailer < ApplicationMailer\nend")
   end
@@ -46,7 +48,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects mailers defined using Class.new" do
     source = 'MyMailer = Class.new(ActionMailer::Base)'
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq('MyMailer = Class.new(ApplicationMailer)')
   end
@@ -54,7 +56,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects nested mailers defined using Class.new" do
     source = 'Nested::MyMailer = Class.new(ActionMailer::Base)'
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq('Nested::MyMailer = Class.new(ApplicationMailer)')
   end
@@ -62,7 +64,7 @@ describe RuboCop::Cop::Salsify::RailsApplicationMailer do
   it "corrects anonymous mailers" do
     source = 'Class.new(ActionMailer::Base) {}'
     inspection = inspect_source(source)
-    expect(inspection[0].message).to eq(described_class::MSG)
+    expect(inspection[0].message).to eq(message)
     expect(inspection[0].location.source).to eq(highlights)
     expect(autocorrect_source(source)).to eq('Class.new(ApplicationMailer) {}')
   end
